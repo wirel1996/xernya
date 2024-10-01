c6e2167863b227ff63f2e9e9f18b321c9b947fa0
Vladislav <wirel1996@mail.ru>
2024-10-01 22:39:18 +0700

    Добавил колбэки и валидацию

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+    validates :price, numericality: { greater_than: 0, allow_nil: true }
+    validates :name, :description, presence: true
+
+    after_initialize { p 'initialize'}
+    after_save       {p 'save'}
+    after_create     {p 'create'}
+    after_update     {p 'update'}
+    after_destroy    {p 'destroy'}
 end
