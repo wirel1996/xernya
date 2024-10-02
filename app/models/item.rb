214236cde596cd1880df3c9dfecb7efc6ab9c368
Vladislav <wirel1996@mail.ru>
2024-10-02 21:42:24 +0700

    Добавил параметры фильрации

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
     validates :price, numericality: { greater_than: 0, allow_nil: true }
-    validates :name, :description, presence: true
+    validates :name, presence: true
 
     after_initialize { p 'initialize'}
     after_save       {p 'save'}
     after_create     {p 'create'}
     after_update     {p 'update'}
     after_destroy    {p 'destroy'}
+    
 end
