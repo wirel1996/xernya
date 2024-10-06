8fcd8027c62ca20ae8402f91b60a162f0ac6b00b
Vladislav <wirel1996@mail.ru>
2024-10-06 23:02:28 +0700

    Dobavil action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
         layout false
         skip_before_action :verify_authenticity_token
+        before_action :find_item, only: %i[show edit update destroy]
+        before_action :admin?, only: %i[edit update new create destroy]
+        after_action :show_info, only: %i[index]
 
         def index
         @items=Item.all
@@ -16,35 +19,29 @@ class ItemsController < ApplicationController
     end
 
     def show
-        unless (@item = Item.where(id: params[:id]).first)
-            render body: 'Page not found', status: 404
+            render body: 'Page not found', status: 404 unless @item
          end
-end
 
     def edit
-        unless (@item = Item.where(id: params[:id]).first)
-            render body: 'Page not found', status: 404
+        render body: 'Page not found', status: 404 unless @item
         end
-    end
 
     def new; end
 
     def update
-        item = Item.where(id: params[:id]).first
-        if item.update(items_params)
+        if @item.update(items_params)
             redirect_to items_path
             else
-                render json: item.errors, status: :unprocessable_entity
+                render json: @item.errors, status: :unprocessable_entity
             end       
     end
 
 
     def destroy
-       item = Item.where(id: params[:id]).first.destroy
-       if item.destroyed?
+       if @item.destroy.destroyed?
         redirect_to items_path
        else
-        render json: item.errors, status: :unprocessable_entity
+        render json: @item.errors, status: :unprocessable_entity
        end
      end
 
@@ -54,4 +51,17 @@ end
             params.permit(:name, :price, :real, :weight,
             :description )
         end
-end
+
+        def find_item
+            @item = Item.where(id: params[:id]).first
+        end
+
+        def admin?
+            render json:'Access denied', status: :forbidden unless params[:admin]
+        end
+
+        def show_info
+            puts 'Index edpoint'
+        end
+        
+    end
