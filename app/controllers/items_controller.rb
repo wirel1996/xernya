1180b963e3abe15709f7012fb540f15c1395d740
Vladislav <wirel1996@mail.ru>
2024-10-06 22:11:24 +0700

    Добавил все рест фул контроллеры

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
         def create 
             item = Item.create(items_params)
             if item.persisted?
-                render json: item.name, status: :created 
+                redirect_to items_path
             else
                 render json: item.errors, status: :unprocessable_entity
         end
